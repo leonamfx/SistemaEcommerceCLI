ca36c4f78a20a90f1abfbeb32c68db82e0b2241f
LeonamLima <leonamfx@gmail.com>
2019-11-23 16:23:02 -0300

    Created Cliente Class

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClienteListComponent } from './cliente-list/cliente-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ClienteListComponent]
 })
 export class ClientesModule { }
