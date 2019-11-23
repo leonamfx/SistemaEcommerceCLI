5654a7536921c5da944e22736c41d830ef03e78f
LeonamLima <leonamfx@gmail.com>
2019-11-23 16:18:53 -0300

    Created Cliente Module

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -10,5 +10,9 @@ const routes: Routes = [
         path: 'home',
         component: HomeComponent
     },
+    { 
+        path: 'clientes',
+        loadChildren: 'app/clientes/clientes.module#ClientesModule'
+    }
   ];
 export const RoutingModule = RouterModule.forRoot(routes);
