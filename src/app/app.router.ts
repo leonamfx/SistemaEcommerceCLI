1a8fffc113be5ea4f376c40c789a5590c4ab801c
LeonamLima <leonamfx@gmail.com>
2019-11-23 16:13:34 -0300

    Criacao Router.ts

---
+++ src/app/app.router.ts
@@ -0,0 +1,14 @@
+
+import { Routes, RouterModule } from '@angular/router'
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+  ];
+export const RoutingModule = RouterModule.forRoot(routes);
